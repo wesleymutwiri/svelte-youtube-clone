dc3cced37f8731b65543a9404784c2d24bd90a47
wesleymutwiri <wesleymochiemo@gmail.com>
2020-05-20 00:58:22 +0300

    few changes to the basic application layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 	import Sidebar from '../components/SideBar.svelte';
+	import Navbar from '../components/TopNav.svelte';
+
 	export let segment;
 </script>
 
@@ -8,15 +10,44 @@
 	main {
 		width: 70%;
 		margin-left: 310px;
+		/* margin-top: 15px; */
+	}
+
+	.sidebar {
+		width: 30%;
 	}
 
 	body {
 		width: 100%;
 	}
+
+	.navbar {
+		/* margin-left: 310px; */
+	}
+
+	@media screen and (max-width: 1020px) {
+		.sidebar {
+			display: none;
+		}
+
+		main {
+			width: 100%;
+			margin-left: 0px;
+		}
+
+		.navbar {
+			margin-left: 0px;
+		}
+	}
 </style>
-<Sidebar />
-<!-- <Nav {segment} /> -->
+<div class="sidebar">
+	<Sidebar />
+</div>
 
 <main>
+	<div class="navbar">
+		<Nav {segment} />
+	</div>
+	<!-- <Navbar /> -->
 	<slot></slot>
 </main>
