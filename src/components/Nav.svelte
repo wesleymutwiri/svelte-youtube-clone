1b9201e08841774926ebb8690a654e0aa5185e6a
wesleymutwiri <wesleymochiemo@gmail.com>
2020-05-20 01:03:11 +0300

    trials at successfully implementing the top navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+		border-bottom: 1px solid #fff;
 		font-weight: 300;
 		padding: 0 1em;
 	}
@@ -24,6 +24,7 @@
 	li {
 		display: block;
 		float: left;
+		text-transform: capitalize;
 	}
 
 	.selected {
@@ -36,7 +37,7 @@
 		content: '';
 		width: calc(100% - 1em);
 		height: 2px;
-		background-color: rgb(255, 62, 0);
+		background-color: #ff3465;
 		display: block;
 		bottom: -1px;
 	}
---
+++ src/components/TopNav.svelte
@@ -0,0 +1,67 @@
+<nav>
+    <div class="container-1">
+        <form action="" method="get">
+            <span class="icon"><i class="fa fa-search"></i></span>
+            <input type="search" name="search" id="search" placeholder="Search...">
+        </form>
+
+    </div>
+</nav>
+
+<style>
+    nav {
+        width: 100%;
+    }
+
+    .container-1 {
+        width: 300px;
+        vertical-align: middle;
+        white-space: nowrap;
+        position: relative;
+    }
+
+    .container-1 input#search {
+        width: 300px;
+        height: 50px;
+        background: #2b303b;
+        border: none;
+        font-size: 10pt;
+        float: left;
+        color: #63717f;
+        padding-left: 45px;
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        border-radius: 5px;
+    }
+
+    .container-1 input#search::-webkit-input-placeholder {
+        color: #65737e;
+    }
+
+    .container-1 input#search:-moz-placeholder {
+        /* Firefox 18- */
+        color: #65737e;
+    }
+
+    .container-1 input#search::-moz-placeholder {
+        /* Firefox 19+ */
+        color: #65737e;
+    }
+
+    .container-1 input#search:-ms-input-placeholder {
+        color: #65737e;
+    }
+
+    .container-1 input#search:-ms-input-placeholder {
+        color: #65737e;
+    }
+
+    .container-1 .icon {
+        position: absolute;
+        top: 50%;
+        margin-left: 17px;
+        margin-top: 17px;
+        z-index: 1;
+        color: #4f5b66;
+    }
+</style>
