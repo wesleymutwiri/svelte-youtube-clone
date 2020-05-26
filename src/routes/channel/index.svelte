cc5877fffdaa6e46f71bdcb367ccba8343ae1330
wesleymutwiri <wesleymochiemo@gmail.com>
2020-05-26 12:43:23 +0300

    added auth, channel files and video file

---
+++ src/routes/channel/index.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+        <img src="#" alt="#" class="display-picture"> <span>Wesley Mochiemo</span>
+    </div>
+    <!-- <Videos /> -->
+</div>
+
+<style>
+    .display-picture {
+        border-radius: 50%;
+        height: 16px;
+        width: 16px;
+        cursor: pointer;
+    }
+</style>
---
+++ src/routes/video.svelte
@@ -0,0 +1,5 @@
+<script>
+    import VideoPlayer from '../components/VideoPlayer.svelte';
+</script>
+
+<VideoPlayer />
